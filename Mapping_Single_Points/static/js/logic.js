c1dac1019830141a3257908ad58f7dc8e09a60cf
fathi129 <nasreen.fathi@gmail.com>
2022-06-22 16:48:52 -0400

    Single point is added

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,20 +1,22 @@
 console.log('working..!!');
 //  var map = L.map('mapid').setView([51.505,-0.09],13);
- let map = L.map('mapid').setView([40.7, -94.5], 4);
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: API_KEY
-// });
+ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
 
 
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
 
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
-L.circle([34.0522, -118.2437], {
-    radius: 100
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color:"black",
+    fillColor:'#ffffa1'
  }).addTo(map);
